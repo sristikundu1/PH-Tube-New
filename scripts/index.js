38d964b2bbb7b78d051cb40c71f69e573e4b6242
Sristi <sristikundu2468@gmail.com>
2026-02-24 00:48:09 +0600

    fix the active button and show card according to category

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
+// remove previous active button 
+function removeActiveClass() {
+  const activeClass = document.getElementsByClassName("active");
+
+  for (let btn of activeClass) {
+    btn.classList.remove("active");
+  }
+}
+
+
+// convert second to hour-sec-min time formate
+
+function formatTimeAgo(seconds) {
+  const totalSeconds = parseInt(seconds);
+
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+
+  return `${hours}hrs ${minutes} min ago`;
+}
+
 function loadCategory() {
   // fetch the category data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -14,44 +35,52 @@ function displayCategory(categories) {
 
     //   show data in button
     categorydiv.innerHTML = `
-        <button class="btn btn-sm bg-[rgba(37,37,37,0.15)] text-[rgba(37,37,37,0.7)] hover:bg-[var(--color-primary)] hover:text-white">
+        <button id="btn-${cat.category_id}" onclick='loadCategoriesVideo( ${cat.category_id})' class="btn btn-sm bg-[rgba(37,37,37,0.15)] text-[rgba(37,37,37,0.7)] hover:bg-[var(--color-primary)] hover:text-white">
     ${cat.category}
   </button>
       `;
 
-    //   append the div 
+    //   append the div
     categoryContainer.appendChild(categorydiv);
   }
 }
 
-// convert second to hour-sec-min time formate
 
-function formatTimeAgo(seconds) {
-  const totalSeconds = parseInt(seconds);
 
-  const hours = Math.floor(totalSeconds / 3600);
-  const minutes = Math.floor((totalSeconds % 3600) / 60);
-
-  return `${hours}hrs ${minutes} min ago`;
-}
-
-// fetch card data 
+// fetch card data
 function LoadVideos() {
-  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-  .then(res => res.json())
-  .then(data => displayVideos(data.videos))
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => {
+      removeActiveClass();
+      document.getElementById('btn-all').classList.add("active");        //all button active class add
+      displayVideos(data.videos)});
 }
 
-// display videos 
+// display videos
 
 const displayVideos = (videos) => {
+  const videoCardContainer = document.getElementById("card-container");
 
-  const videoCardContainer = document.getElementById('card-container');
+  videoCardContainer.innerHTML = "";
 
-  // loop to show videos in card 
+  if (videos.length == 0) {
+    videoCardContainer.innerHTML = `
+    <div class="col-span-full flex flex-col justify-center items-center gap-3">
+          <img class="w-32 h-32" src="./Icon.png" alt="no-data" />
+          <h1
+            class="text-[rgba(23,23,23,1)] font-bold text-2xl leading-[44px] text-center"
+          >
+            Oops!! Sorry, There is no content here
+          </h1>
+        </div>
+    `;
+  }
+
+  // loop to show videos in card
   videos.forEach((video) => {
     // console.log(video.thumbnail)
-    const cardDiv = document.createElement('div');
+    const cardDiv = document.createElement("div");
     cardDiv.innerHTML = `
     <div class="card bg-base-100 shadow-sm">
      <figure class="relative">
@@ -61,7 +90,7 @@ const displayVideos = (videos) => {
                 alt="image"
               />
 
-              ${video.others.posted_date? `<span class="absolute right-2 bottom-2 bg-[rgba(23,23,23,1)] p-2 rounded-md text-white">${formatTimeAgo(video.others.posted_date)}</span>` :''}
+              ${video.others.posted_date ? `<span class="absolute right-2 bottom-2 bg-[rgba(23,23,23,1)] p-2 rounded-md text-white">${formatTimeAgo(video.others.posted_date)}</span>` : ""}
               
               
              
@@ -101,32 +130,29 @@ const displayVideos = (videos) => {
               </div>
             </div>
             </div>
-    `
+    `;
 
     videoCardContainer.appendChild(cardDiv);
   });
+};
 
-}
+// show card according to category button
+const loadCategoriesVideo = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
 
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActiveClass();                  //remove previous active class
+      const clickedButton = document.getElementById(`btn-${id}`);
+      clickedButton.classList.add("active");              // add active class
 
-// {
-//     "category_id": "1001",
-//     "video_id": "aaaa",
-//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-//     "title": "Shape of You",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-//             "profile_name": "Olivia Mitchell",
-//             "verified": ""
-//         }
-//     ],
-//     "others": {
-//         "views": "100K",
-//         "posted_date": "16278"
-//     },
-//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// }
+      displayVideos(data.category);
+    });
+};
 
-LoadVideos();
+// by default all button remain active 
+document.addEventListener("DOMContentLoaded", function () {
+  LoadVideos();
+});
 loadCategory();
